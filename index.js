f08fad8d4cea690265ca7219e47c39535cb1d73f
Akaryatrh <sebastien.vaneyck@gmail.com>
2016-01-24 08:49:41 +0100

    Release 0.2.0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/main');
+#! /usr/bin/env node
+;
+var CliParser, SvgAssets, cliParser, options, svgAssets;
+
+SvgAssets = require('./lib/main');
+
+CliParser = require('./lib/cli-parser');
+
+cliParser = new CliParser();
+
+options = cliParser.initCommander();
+
+if (options != null) {
+  svgAssets = new SvgAssets(options);
+  svgAssets.process();
+}
